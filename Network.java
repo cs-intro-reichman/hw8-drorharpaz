dd8f91d544bd77eef5999fca94491b834b6f1930
drorharpaz <dror.harpaz@post.runi.ac.il>
2025-01-08 20:35:44 +0200

    Net - 5

--- Network.java
+++ Network.java
@@ -140,13 +140,10 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String ans = "Network:";
-       if (this.userCount > 0) {
-        ans += "\n";
-       }
+       StringBuilder ans = new StringBuilder("Network:");
        for (int i = 0; i < userCount; i ++){
-        ans += users[i].toString() + "\n";
+        ans.append("\n" + users[i].toString());
        }
-       return ans;
+       return ans.toString();
     }
 }
